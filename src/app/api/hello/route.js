588f961d0e4297c7e2dd85e3a4e1dda34686de11
Najtex <timi.kompi@gmail.com>
2023-10-04 13:50:39 +0200

    Fixed homepage

--- src/app/api/hello/route.js
+++
@@ -1,7 +0,0 @@
-import prisma from "../../../../prisma/client";
-
-export async function GET(req, res) {
-  const users = await prisma.user.findMany();
-
-  return new Response(JSON.stringify(users));
-}
